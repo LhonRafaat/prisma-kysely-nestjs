df45e28163b2bc94ba196f295e4a782187e8aa98
lhonRafaat <lhontaekwondo@gmail.com>
2024-10-20 18:30:06 +0300

    refactor: update kysely types

--- prisma/generated/types.ts
+++ prisma/generated/types.ts
@@ -1,24 +1,30 @@
-import type { ColumnType } from 'kysely';
+import type { ColumnType, Insertable } from 'kysely';
 export type Generated<T> =
   T extends ColumnType<infer S, infer I, infer U>
     ? ColumnType<S, I | undefined, U>
     : ColumnType<T, T | undefined, T>;
 export type Timestamp = ColumnType<Date, Date | string, Date | string>;
 
-export type Post = {
-  id: Generated<number>;
-  title: string;
-  content: string | null;
-  published: Generated<number>;
-  author_id: number;
-};
+export const Role = {
+  USER: 'USER',
+  ADMIN: 'ADMIN',
+  MODERATOR: 'MODERATOR',
+  GUEST: 'GUEST',
+} as const;
+export type Role = (typeof Role)[keyof typeof Role];
 export type User = {
   id: Generated<number>;
   email: string;
+  password: string;
   name: string | null;
   refresh_token: string | null;
 };
+export type UserRole = {
+  id: Generated<number>;
+  user_id: number;
+  role: Role;
+};
 export type DB = {
-  posts: Post;
+  user_role: UserRole;
   users: User;
 };
