7d1f63a12d99ed75557480ce1fd75fa059666fee
lhonRafaat <lhontaekwondo@gmail.com>
2025-01-04 20:23:30 +0300

    feat: update updateuserdto

--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/swagger';
+import { OmitType, PartialType } from '@nestjs/swagger';
 import { RegisterDto } from '../../auth/dto/register.dto';
 
-export class UpdateUserDto extends PartialType(RegisterDto) {}
+export class UpdateUserDto extends PartialType(
+  OmitType(RegisterDto, ['password', 'roles'] as const),
+) {}
